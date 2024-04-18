683e5b8bb042ee5c5b7c92570dd831215babea61
Benvaz722 <benvaz722@gmail.com>
2024-04-18 15:22:37 -0500

    add color

--- src/GhostTool.py
+++ src/GhostTool.py
@@ -1,5 +1,7 @@
 import maya.cmds as mc
-from PySide2.QtWidgets import QWidget,QVBoxLayout, QLabel, QPushButton, QHBoxLayout, QListWidget, QAbstractItemView
+from PySide2.QtCore import Signal
+from PySide2.QtWidgets import QWidget,QVBoxLayout, QLabel, QPushButton, QHBoxLayout, QListWidget, QAbstractItemView, QColorDialog
+from PySide2.QtGui import QColor, QPainter, QBrush
 
 def GetCurrentFrame():
     return int(mc.currentTime(q=True))#query
@@ -10,13 +12,25 @@ class Ghost:
         self.ghostGrp = "ghost_grp"
         self.frameAttr = "frame"
         self.srcAttr = "src"
+        self.color = [0,0,0]
 
         self.InitIfGhostGrpNotExist()
+
+    def UpdateGhostColors(self, color: QColor):
+        ghosts = mc.listRelatives(self.ghostGrp, c=True)
+        self.color[0] = color.redF()
+        self.color[1] = color.greenF()
+        self.color[2] = color.blueF()
+        
+        for ghost in ghosts:
+            mat = self.GetMaterialNameForGhost
+            mc.setAttr(mat+".color", color.redF(), color.greenF(), color.blueF(),type = "doubles3" )
 
     def InitIfGhostGrpNotExist(self):
         if mc.objExists(self.ghostGrp):
             storedSrcMeshes = mc.getAttr(self.ghostGrp + "." + self.srcAttr)
-            self.srcMeshes = set(storedSrcMeshes.split(","))
+            if storedSrcMeshes:
+                self.srcMeshes = set(storedSrcMeshes.split(","))
             return
         
         mc.createNode("transform", n = self.ghostGrp)
@@ -35,7 +49,7 @@ class Ghost:
 
     def addGhost(self):
         for srcMesh in self.srcMeshes:
-            currentFrame = GetCurrentFrame
+            currentFrame = GetCurrentFrame()
             ghostName = srcMesh + "_" + str(currentFrame)
             if mc.objExists(ghostName):
                 mc.delete(ghostName)
@@ -44,21 +58,95 @@ class Ghost:
             mc.parent(ghostName, self.ghostGrp)
             mc.addAttr(ghostName, ln = self.frameAttr, dv = currentFrame)#default value
 
+            matName = self.GetMaterialNameForGhost(ghostName)#figure out the name for the material
+            if not mc.objExists(matName): #check if material not exist
+                mc.shadingNode("lambert", asShader = True, name = matName)#creat teh lambert mat if not exist
+
+            sgName = self.GetShadingEnginForGhost(ghostName) #figure out the name of the shading engeine
+            if not mc.objExists(sgName): #check if the shading engine exist
+                mc.sets(name = sgName, renderable = True, empty = True) #creat teh shading if not exist
+
+            mc.connectAttr(matName + ".outColor", sgName + ".surfaceShader", force = True)# connect the material to the shading engine
+            mc.sets(ghostName, edit=True,forceElement = sgName)#asign material to ghost
+
+            mc.setAttr(matName+".color", self.color[0],self.color[1],self.color[2],type="double3")              
+    def GetMaterialNameForGhost(self, ghost):
+        return ghost + "_mat"
+
+    def GetShadingEnginForGhost(self, ghost):
+        return ghost + "_sg"
+    
     def GoToNextGhost(self):
-        frames = self.GetGhostFramesSorted()
-        currentFrame = GetCurrentFrame
-        for frame in frames:
-            if frame > currentFrame:
+        frames = self.GetGhostFramesSorted()#find all the frames we have in ascending order
+        if not frames: # if there is not frames, there is not ghost, do nothing
+            return
+        currentFrame = GetCurrentFrame()
+        for frame in frames:#find all the frames we have in ascending order
+            if frame > currentFrame:#if we find one that is bigger than the current frame, it should be where we move time slider to 
                 mc.currentTime(frame, e = True)#e means edit, we are editing teh time slider to at frame
                 return
         mc.currentTime(frames[0], e=True)
 
     def GotToPrevGhost(self):
-        pass
+        frames = self.GetGhostFramesSorted()
+        if not frames:
+            return
         
+        currentFrame = GetCurrentFrame()
+        frames.reverse()
+        for frame in frames:
+            if frame < currentFrame:
+                mc.currentTime(frame, e = True)
+                return
+            
+        mc.currentTime(frames[0], e = True)
+    
+    def DeleteGhostOnCurrentFrame(self):
+        #frames = self.GetGhostFramesSorted()
+        #if not frames:
+           #return
+
+       # currentFrame = GetCurrentFrame()
+        #for srcMesh in self.srcMeshes:
+            #ghostName = srcMesh + "_" + str(currentFrame)
+            #if mc.objExists(ghostName):
+                #mc.delete(ghostName)
+
+        currentFrames = GetCurrentFrame()
+        ghosts = mc.listRelatives(self.ghostGrp, c = True)#gets all childer of the ghost grp
+        for ghost in ghosts:
+            ghostFrame = mc.getAttr(ghost+"."+self.frameAttr)#ask for the frame recorded for the ghost
+            if ghostFrame == currentFrames: #if the ghost frame is the sames as current frame
+                self.DeletGhost(ghost)#remove ghost'
+
+    def DeletGhost(self, ghost):
+        #delete material
+        mat = self.GetMaterialNameForGhost(ghost)
+        if mc.objExists(mat):
+            mc.delete(mat)
+
+        #delete shading engine
+        sg = self. GetShadingEnginForGhost(ghost)
+        if mc.objExists(sg):
+            mc.delete(sg)
+
+        #delete the ghost model
+        if mc.objExists(self.ghostGrp):
+            mc.delete(ghost)
+
+    def DeleteGhostOnAllFrames(self):
+        ghosts = mc.listRelatives(self.ghostGrp, c= True)
+        for ghost in ghosts:
+            self.DeletGhost(ghost)    
+
     def GetGhostFramesSorted(self):
         frames = set()
-        for ghost in mc.listRelatives(self.ghostGrp, c=True):
+        ghosts = mc.listRelatives(self.ghostGrp, c=True)
+
+        if not ghosts:
+            return[]
+        
+        for ghost in ghosts:
             frame = mc.getAttr(ghost + "." + self.frameAttr)
             frames.add(frame)
 
@@ -66,6 +154,24 @@ class Ghost:
         frames.sort()#this sort the frames in accending order
         return frames#return sorted frames
     
+class ColorPicker(QWidget):
+    onColorChange = Signal(QColor)#this adds a built in class memeber called oncolorchange
+    def __init__(self, width = 30, hight = 20):
+        super().__init__()
+        self.setFixedSize(width, hight)
+        self.color = QColor()
+
+    def mousePressEvent(self, event):
+        color = QColorDialog().getColor(self.color)
+        self.color = color
+        self.onColorChange.emit(self.color)
+        self.update()#update the widget
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setBrush(QBrush(self.color))
+        painter.drawRect(0,0,self.width(), self.height())
+
 class GhostWidget(QWidget):
     def __init__(self):
         super().__init__()#need to call if you are inheriting from parent class
@@ -92,12 +198,29 @@ class GhostWidget(QWidget):
         self.ctrlLayout.addWidget(addGhostBtn)
 
         prevGhostBtn = QPushButton("Prev")
-        prevGhostBtn.clicked.connect(self.ghost.GoToPrevGhost)
+        prevGhostBtn.clicked.connect(self.ghost.GotToPrevGhost)
         self.ctrlLayout.addWidget(prevGhostBtn)
 
         nextGhostBtn = QPushButton("Next")
         nextGhostBtn.clicked.connect(self.ghost.GoToNextGhost)
         self.ctrlLayout.addWidget(nextGhostBtn)
+
+        self.ctrlLayout = QHBoxLayout()
+        self.masterLayout.addLayout(self.ctrlLayout)
+
+        DelGhostBtn = QPushButton("Delete Current Ghost")
+        DelGhostBtn.clicked.connect(self.ghost.DeleteGhostOnCurrentFrame)
+        self.ctrlLayout.addWidget(DelGhostBtn)
+
+        DelAllGhostBtn = QPushButton("Delete all Ghost")
+        DelAllGhostBtn.clicked.connect(self.ghost.DeleteGhostOnAllFrames)
+        self.ctrlLayout.addWidget(DelAllGhostBtn)
+
+        colorPicker = ColorPicker()
+        colorPicker.onColorChange.connect(self.ghost.UpdateGhostColors)
+        self.masterLayout.addWidget(colorPicker)
+
+
         
     def SrcMeshSelectionChanged(self):
         mc.select(cl=True)#cancle everything
